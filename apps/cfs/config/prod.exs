a5cd1e0ecd41d544ad5075e837126f3f9c3e90a8
matthewoden <heymatthewoden@gmail.com>
2017-04-22 14:49:21 -0500

    pull secret out of .secret

--- apps/cfs/config/prod.exs
+++ apps/cfs/config/prod.exs
@@ -4,6 +4,15 @@ use Mix.Config
 config :cfs, :store, cache_policy: nil
 
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+
+config :ex_aws,
+  access_key_id: System.get_env("GUNSHOTS_AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("GUNSHOTS_AWS_SECRET_ACCESS_KEY"),
+  region: "us-east-1"
+
+
+config :cfs,
+  dynamo: [table_name: "gunshots-or-fireworks"],
+  store:  [cache_policy: :fetch]
+
+
